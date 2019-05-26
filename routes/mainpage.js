2f51e4438dfe849aae13f4aca0b077bac7a96bd9
Altuğ Buğra Çelenk <bugracelenk01@gmail.com>
2019-05-26 19:24:58 +0300

    little typo fixed

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const MainPageController = require("../controllers/mainpage");
 
-router.get("/signup", MainPageController.get_services);
+router.get("/getservices", MainPageController.get_services);
 
 module.exports = router;
