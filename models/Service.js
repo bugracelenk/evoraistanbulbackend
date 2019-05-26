15c9d8e4b9cbc4d08e39a59a76b317a7772f6223
Altuğ Buğra Çelenk <bugracelenk01@gmail.com>
2019-05-26 19:53:21 +0300

    trying to fix this

--- models/Service.js
+++ models/Service.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-const Service = mongoose.model("Service",new mongoose.Schema({
-    name: String,
-    image: String
-  })
-);
+const Service = mongoose.model('Service', new mongoose.Schema({
+  name: String,
+  image: String
+}));
 
 module.exports = Service;
